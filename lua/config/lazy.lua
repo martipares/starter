3aac3b7c0a6785e00d244cd510a256b1994a6bef
martipares <martiparesmath@gmail.com>
2025-05-11 11:33:43 +0200

    my options and extras

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,6 +20,11 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     -- { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { "martiperes/LazyVim", import = "lazyvim.plugins" },
+    -- import any extras modules here
+    -- { import = "lazyvim.plugins.extras.lang.tex" },
+    -- { import = "lazyvim.plugins.extras.coding.luasnip" },
+    -- { import = "lazyvim.plugins.extras.util.dot" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.wrap = true
+
+-- vim.opt.virtualedit = "onemore"
+
+vim.opt.relativenumber = false
+
+vim.opt.spelllang = "en_gb"
+-- vim.opt.spelllang = "es_es"
+-- vim.opt.spelllang = "ca_es"
+
+vim.opt.spell = true
