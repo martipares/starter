5bf49d15d58c58937e4c2ba15c027598470965f6
martipares <martiparesmath@gmail.com>
2025-05-21 23:23:32 +0200

    change theme for fork version

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,16 +1,16 @@
 return {
-  { "folke/tokyonight.nvim" },
+  { "martipares/tokyonight.nvim" },
   {
-    "folke/tokyonight.nvim",
+    "martipares/tokyonight.nvim",
     lazy = true,
     -- theme tokyonight.nvim moon, storm, night, day
     opts = {
       style = "moon",
-      on_colors = function(colors)
-        colors.fg = "#ffffff"
-        colors.fg_dark = "#bbbbbb"
-        -- colors.fg_gutter = "#999999"
-      end,
+      -- on_colors = function(colors)
+      --   -- colors.fg = "#ffffff"
+      --   -- colors.fg_dark = "#bbbbbb"
+      --   -- colors.fg_gutter = "#999999"
+      -- end,
     },
   },
 }
