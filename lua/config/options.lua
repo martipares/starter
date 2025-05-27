ea3046271aa44ccd6430be2072eb017f2e8ab801
martipares <martiparesmath@gmail.com>
2025-05-28 00:02:28 +0200

    new spell files and config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,10 +8,21 @@ vim.opt.virtualedit = "onemore"
 
 vim.opt.relativenumber = false
 
-vim.opt.spelllang = "en_gb"
--- vim.opt.spelllang = "es_es"
--- vim.opt.spelllang = "ca_es"
-
 vim.opt.spell = true
 
 vim.g.lazyvim_picker = "snacks"
+
+-- Englesh GB en_gb, en_GB
+vim.opt.spelllang = "en_gb"
+-- Castella es_es, es_ES
+-- vim.opt.spelllang = "es_es"
+-- Catala ca_ES, ca_es
+-- vim.opt.spelllang = "ca_es"
+-- Danish (Denmark)
+-- vim.opt.spelllang = "da_dk"
+-- German
+-- vim.opt.spelllang = "de_de"
+-- Dutch (Belgium)
+-- vim.opt.spelllang = "nl_be"
+-- French(Belgium)
+-- vim.opt.spelllang = "fr_be"
